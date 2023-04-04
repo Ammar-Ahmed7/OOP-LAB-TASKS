7b4a717f9952624f8216137be5081f877d044024
Ammar <ammar.ahmed9020@gmail.com>
2023-04-04 12:57:45 +0500

    Updated

--- bushra assign. 2/BasePlusCommissionEmployeeTest.java
+++
@@ -1,6 +0,0 @@
-public class BasePlusCommissionEmployeeTest {
-    public static void main(String[] args) {
-        BasePlusCommissionEmployee b = new BasePlusCommissionEmployee("yahya", "irfan", "123.0", 45.0, 0.1, 500.0);
-        System.out.println(b.toString());
-    }
-}
--- bushra assign. 2/ShapeTest.java
+++
@@ -1,7 +0,0 @@
-public class ShapeTest {
-    public static void main(String[] args) {
-        Rectangle r = new Rectangle("Blue", true, 5.0, 5.0);
-        Circle c = new Circle("Red", true, 5.0);
-
-    }
-}
