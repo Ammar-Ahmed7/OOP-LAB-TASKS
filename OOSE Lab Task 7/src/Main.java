a13eb5365797589d2cbe5f60dd846cc49f09f494
Ammar <ammar.ahmed9020@gmail.com>
2023-04-12 09:03:35 +0500

    Updated

--- OOSE Lab Task 7/src/Main.java
+++ OOSE Lab Task 7/src/Main.java
@@ -5,14 +5,18 @@ import static java.lang.System.exit;
 public class Main {
     public static void main(String[] args) {
         while (true) {
-
             System.out.println("\nEnter your option \n 1.Circle \n 2.Square \n 3.Rectangle \n 4.Exit");
             Scanner sc = new Scanner(System.in);
             int op = sc.nextInt();
+            if(op!=1 || op!=2 || op!=3 || op!=4){
+                System.out.println("\nEnter a valid input \n 1.Circle \n 2.Square \n 3.Rectangle \n 4.Exit");
+                 op = sc.nextInt();
+            }
 
             switch (op) {
-                case 1:
 
+
+                case 1:
                     shape a = new Circle();
                     System.out.println(a);
                     break;
